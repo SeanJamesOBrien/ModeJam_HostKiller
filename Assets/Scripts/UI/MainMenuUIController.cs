9007af81efc2cc7045ecd9dec662dbf6d165571c
SeanJamesOBrien <seanobrien2702@gmail.com>
2023-12-07 12:10:11 -0500

    Added credits to main menu

--- Assets/Scripts/UI/MainMenuUIController.cs
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MainMenuUIController : MonoBehaviour
 {
+    [SerializeField] GameObject creditPanel;
     public void StartGame()
     {
         SceneController.Instance.LoadNextScene(K.GameScene);
+        creditPanel.SetActive(false);
+    }
+
+    public void ToggleCredits()
+    {
+        creditPanel.SetActive(!creditPanel.activeSelf);
     }
 
     public void QuitGame()
